f544fd26ca5d569643684503ed2f1a75d6bbc4ae
claireingham <claireingham75@gmail.com>
2024-12-15 06:46:51 +0100

    Link HTML to Moment
    Update date and time in real time

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-alert("hello");
+setInterval(function () {
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = document.querySelector("#london .date");
+  let londonTimeElement = document.querySelector("#london .time");
+  let londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[<small>]"
+  );
+
+  let madridElement = document.querySelector("#madrid");
+  let madridDateElement = document.querySelector("#madrid .date");
+  let madridTimeElement = document.querySelector("#madrid .time");
+  let madridTime = moment().tz("Europe/Madrid");
+  madridDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  madridTimeElement.innerHTML = madridTime.format(
+    "h:mm:ss [<small>]A[<small>]"
+  );
+}, 1000);
